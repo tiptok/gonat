c1aaee2fbcc8ed680da871bd613cd9ac5710a88c
yangfu <785409885@qq.com>
2018-06-07 18:45:17 +0800

    协议实体

--- core/biz.go
+++ core/biz.go
@@ -7,6 +7,7 @@ import (
 
 	"fmt"
 
+	"github.com/tiptok/gonat/global"
 	"github.com/tiptok/gonat/model"
 	"github.com/tiptok/gotransfer/comm"
 )
@@ -36,7 +37,47 @@ func (b bizBase) RegistCmd(sCmd string, t reflect.Type) {
 	b.CmdType.Set(sCmd, t)
 }
 
-//BizDBTdf 数据库分发
+//分发接口
+type IBiz interface {
+	SendEntity(obj interface{}) error
+	ReceiveEntity() (interface{}, error)
+	Start() error
+}
+
+/*BizDBTdf 数据库分发*/
 type BizDBTdf struct {
 	bizBase
+	TaskRec  *global.TaskManager
+	TaskSend *global.TaskManager
+}
+
+//SendEntity 分发数据
+func (b *BizDBTdf) SendEntity(obj interface{}) error {
+	return nil
+}
+func (b *BizDBTdf) onSendEntity(obj interface{}) {
+
+}
+
+//Start 启动
+func (b *BizDBTdf) Start() error {
+	b.bizBase.OnRegistCmdEntity() //注册json 反序列化列表
+	return nil
+}
+
+//ReceiveEntity 接收数据
+func (b *BizDBTdf) ReceiveEntity() (interface{}, error) {
+	return nil, nil
+}
+func (b *BizDBTdf) onReceiveEntity(obj interface{}) {
+
+}
+
+//NewBizDBTdf 新建一个BizDBTdf
+func NewBizDBTdf() *BizDBTdf {
+	biz := &BizDBTdf{}
+	biz.bizBase = bizBase{}
+	biz.TaskRec = global.NewTaskManager("BizDBTdf.Rec.Task", 1000, 1, biz.onReceiveEntity)
+	biz.TaskSend = global.NewTaskManager("BizDBTdf.Send.Task", 1000, 1, biz.onReceiveEntity)
+	return biz
 }
---
+++ model/jtb809/down/DOWN_PLATFORM_MSG.go
@@ -0,0 +1,12 @@
+package down
+
+import (
+	"github.com/tiptok/gonat/model"
+)
+
+//DOWN_PLATFORM_MSG 从链路平台间信息交换
+type DOWN_PLATFORM_MSG struct {
+	model.EntityBase
+}
+
+
